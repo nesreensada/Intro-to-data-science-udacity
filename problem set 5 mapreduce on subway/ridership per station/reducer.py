49413ad10c72d2e5704190552823aaa70116177b
nesreensada <31444316+nesreensada@users.noreply.github.com>
2017-10-11 20:31:52 +0300

    Add files via upload

---
+++ problem set 5 mapreduce on subway/ridership per station/reducer.py
@@ -0,0 +1,39 @@
+import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def reducer():
+    '''
+    Given the output of the mapper for this exercise, the reducer should PRINT 
+    (not return) one line per UNIT along with the total number of ENTRIESn_hourly 
+    over the course of May (which is the duration of our data), separated by a tab.
+    An example output row from the reducer might look like this: 'R001\t500625.0'
+
+    You can assume that the input to the reducer is sorted such that all rows
+    corresponding to a particular UNIT are grouped together.
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    Note that, unlike print, logging.info will take only a single argument.
+    So logging.info("my message") will work, but logging.info("my","message") will not.
+    '''
+
+    stations={}
+    
+    for line in sys.stdin:
+        l=line.split('\t')
+        if l[0] in stations:
+            stations[l[0]]+=float(l[1])
+        else:
+            stations[l[0]]=float(l[1])
+            
+    for key,value in stations.iteritems():
+        print key+'\t'+str(value)  
+        
+reducer()
